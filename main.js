ae67cd57f139b1da4ae409cd1dd8c902243d899c
Bohoaush <sin@boprograms.eu>
2021-06-27 11:10:46 +0200

    minor additions

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ var url = require("url"); // For parsing url parameters
 const {CasparCG} = require("casparcg-connection"); // For CasparCG communication
 const mediainfo = require('node-mediainfo'); // For getting media duration
 
-var currentTime = 0;
+var currentTimeHR = 0; // Variable for human readable time
+var currentTime = 0; // Variable for time in seconds since Jan 1st 1970
+
 var playlist;
-/*var plyNames[];
-var plyPaths[];
-var plyStartModes[];
-var plyStartTimes[];
-var plyStartAts[];
-var plyEndAts[];
-var plyDurations[];*/
+
+var fixedEventsIds = [];
+var fixedEventsTimes = [];
+var fixedEventsAt = 0;
 
 //------------------------------User changeable variables-------------------------------------
 var logLevel = 2; // Set console log level: 0 - error, 1 - warning, 2 - debug, 3 - show time updates too
 var ccg_ip = "127.0.0.1"; // Set CasparCG server Ip address
 var ccg_port = 5250; // Set CasparCG server port
 var interface_port = 8084;
+var media_scanner_port = 8000; // Not used yet
 //--------------------------------------------------------------------------------------------
 
 // Start http interface from file
@@ -95,7 +95,7 @@ function play(channel, layer, itemname) {
     }
 }
 
-function trouble() {
+function trouble() { // Will eventually be used as item to play, if something goes wrong
     ccgtunnel.play(1, 1, "AMB");
 }
 
@@ -105,9 +105,6 @@ function findPathFromPlyId(ply_id, _callback) {
             if (logLevel > 1) {console.log("DEBUG: reading playlist: " + JSON.stringify(playlist)); }
             var returnVar = playlist.PlaylistItems[ply_id].path;
             return returnVar.toString();
-            /*var fuckJavaScript = "js is always screwing with me :(";
-            console.log("BEFORE RETURN: " + fuckJavaScript);
-            return fuckJavaScript;*/
         } else {
             console.log("ERROR: Cannot start playig, no playlist loaded!");
             return false;
@@ -138,66 +135,67 @@ function asignDurationsToPly(plitem, index) {
     var pathToProbe = plitem.path;
     console.log("checking media: " + pathToProbe);
     
+    var itemType = plitem.type;
+    var startMode = plitem.startMode;
+    var startTime = plitem.startTime;
+    var startAt = plitem.startAt;
+    var endAt = plitem.endAt;
     
-    //Fully coppied code, don't know what it does yet
-    /*const getInfos = () => {
-    ccgtunnel.info().then(function(fulfilled){
-        console.log('RESPONSE!');
-        console.log(fulfilled.response.data);
-    },function(reason){
-        console.log("-------REASON------");
-        console.log(reason);
-    });
-    };*/
-    
-    
-    // CINF through casparcg-connection doesn't work at the moment :(
-    // TODO try _callback again
-    //var mediaInfo = await ccgtunnel.cinf(pathToProbe);
-    
-    // Using http GET to media-scanner directly instead | I probably messed something up, doesn't work either...
-    
-    var duration = 0;
-    
-    const options = {
-        hostname: '127.0.0.1',
-        port: 8000,
-        path: '/cinf/' + pathToProbe,
-        method: 'GET'
+    if (startMode == "fixed" && startTime != "" && startTime != "undefined" && startTime != "0") {
+        if (logLevel > 1) { console.log("DEBUG: Adding event to fixed events"); }
+        fixedEventsIds.push(index);
+        fixedEventsTimes.push(startTime);
+    } else if (startMode == "fixed") {
+        if (logLevel > 0) { console.log("WARNING: Found event in fixed mode without valid start time, ignoring"); }
     }
+    
+    if (itemType == "clip") {
+    
+        if (endAt != "auto" && logLevel > 0) { console.log("WARNING: Editing duration on clips not yet supported!"); }
+        
+        var duration = 0;
+        
+        const options = {
+            hostname: '127.0.0.1',
+            port: 8000,
+            path: '/cinf/' + pathToProbe,
+            method: 'GET'
+        }
 
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
+        const req = http.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`)
 
-        res.on('data', d => {
-            process.stdout.write(d);
-            mediaInfoArray = (d + '').split(" ");
-            if (logLevel > 1) { console.log(mediaInfoArray); }
-            var framerate = mediaInfoArray[9].replace('1/','')
-            var framecount = mediaInfoArray[8];
-            if (logLevel > 1) { console.log("DEBUG: frame count: " + framecount + " / framerate: " + framerate); }
-            var duration = (framecount / framerate);
-            if (logLevel > 1) { console.log("DEBUG: Returning duration " + duration); }
-            return duration;
-            
+            res.on('data', d => {
+                process.stdout.write(d);
+                mediaInfoArray = (d + '').split(" ");
+                if (logLevel > 1) { console.log(mediaInfoArray); }
+                var framerate = mediaInfoArray[9].replace('1/','')
+                var framecount = mediaInfoArray[8];
+                if (logLevel > 1) { console.log("DEBUG: frame count: " + framecount + " / framerate: " + framerate); }
+                var duration = (framecount / framerate);
+                if (logLevel > 1) { console.log("DEBUG: Returning duration " + duration); }
+                //return duration;
+                plitem.duration = duration;
+                var plyWriteback = JSON.stringify(playlist);
+                fs.writeFile('playlist_template_aed.json', plyWriteback, (err) => {
+                    if (err) throw err;
+                    if ( logLevel > 1 ) { console.log('Data written to file'); }
+                });
+                
+            })
         })
-    })
 
-    req.on('error', error => {
-        console.error(error);
-        return false;
-    })
+        req.on('error', error => {
+            console.error(error);
+            return false;
+        })
 
-    req.end()
-    
-    //console.log(mediaInfo);
-    
-    
-    //console.log(fullMediaInfo);
-    //plitem.duration = 
+        req.end()
+    } 
+        
 }
 
-function getDurationFromMediaInfo(mediaInfoString, _callback) {
+function getDurationFromMediaInfo(mediaInfoString, _callback) { // Not used?
     mediaInfoArray = mediaInfoString.split(" ");
     if (logLevel > 1) { console.log(mediaInfoArray); }
     // 0 - status, 1 - OSCpath, 2 - statusVerbal, 3 - space, 4 - type (MOVIE), 5 - space,
@@ -207,35 +205,6 @@ function getDurationFromMediaInfo(mediaInfoString, _callback) {
     
     _callback();
 }
-
-/*function getMediaInfoViaHttp (pathToProbe, _callback) {
-    
-    const options = {
-        hostname: '127.0.0.1',
-        port: 8000,
-        path: '/cinf/' + pathToProbe,
-        method: 'GET'
-    }
-
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-
-        res.on('data', d => {
-            process.stdout.write(d);
-            return d;
-        })
-    })
-
-    req.on('error', error => {
-        console.error(error);
-        return false;
-    })
-
-    req.end()
-    
-    
-    _callback();
-}*/ //Isn't used?
 
 
 
@@ -260,7 +229,22 @@ function checkTime() {
     var second = timeNow.getSeconds();
     if (second < 10) { second = "0" + second; }
     
-    currentTime = timeNow.getFullYear().toString() + month + day + hour + minute + second;
+    currentTimeHR = timeNow.getFullYear().toString() + month + day + hour + minute + second;
+    currentTime = (timeNow.getTime() / 1000);
     
     if (logLevel > 2) { console.log("DEBUG: Time: " + currentTime); }
+    
+    if (if fixedEventsIds.length > 0) {
+        if (fixedEventsTimes[fixedEventsAt] >= currentTimeHR) {
+            var playPath = findPathFromPlyId(fixedEventsIds[fixedEventsAt]);
+            console.log("AFTER RETURN: " + playPath);
+            if (playPath != false) {
+                play(1, 1, playPath);
+                console.log("Started fixed event " + playPath);
+                fixedEventsAt += 1;
+            } else {
+                console.log("ERROR: Can't find path for item " + itemid);
+            }
+        }
+    }
 }
